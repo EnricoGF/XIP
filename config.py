f4ccd67ac8818580be322b3d8235486e3f2c77ec
EnricoGF <enricogaforte@gmail.com>
2025-11-03 15:19:56 -0300

    Atualizando README e alguns detalhes do banco de dados

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'chave-secreta-flask'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/financeflow.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../instance/xip.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
